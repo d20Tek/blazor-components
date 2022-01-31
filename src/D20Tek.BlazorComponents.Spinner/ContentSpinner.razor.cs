dcb88a292cb9d4d000caff2c71ab932acc282ef6
Pedro Silva <pedrosi@microsoft.com>
2022-01-31 15:03:16 -0800

    refactor shared properties and logic into SpinnerBase class to use between Spinner and ContentSpinner components.

---
+++ src/D20Tek.BlazorComponents.Spinner/ContentSpinner.razor.cs
@@ -0,0 +1,12 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek. All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.BlazorComponents.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace D20Tek.BlazorComponents
+{
+    public partial class ContentSpinner : ComponentBase
+    {
+    }
+}
