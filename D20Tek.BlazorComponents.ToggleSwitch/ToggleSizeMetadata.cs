f0d8b5c5a794280dfa4a6b8539c418221cea7090
Pedro Silva <pedrosi@microsoft.com>
2022-06-10 23:02:40 -0700

    Updated release pipeline. Created initial ToggleSwitch project. Implemented basic ToggleSwitch functionality. Created ToggleSwitchPage sample code.

---
+++ D20Tek.BlazorComponents.ToggleSwitch/ToggleSizeMetadata.cs
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek. All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+
+namespace D20Tek.BlazorComponents
+{
+    internal class ToggleSizeMetadata
+    {
+        private static IDictionary<Size, string> _elements = new Dictionary<Size, string>
+        {
+            { Size.None, "toggle-md" },
+            { Size.ExtraSmall, "toggle-xs" },
+            { Size.Small, "toggle-sm" },
+            { Size.Medium, "toggle-md" },
+            { Size.Large, "toggle-lg" },
+            { Size.ExtraLarge, "toggle-xl" },
+        };
+
+        public static string GetSizeCss(Size size) => _elements[size];
+    }
+}
---
+++ D20Tek.BlazorComponents.ToggleSwitch/ToggleSwitch.razor.cs
@@ -0,0 +1,60 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek. All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.BlazorComponents.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace D20Tek.BlazorComponents
+{
+    public partial class ToggleSwitch : BaseComponent
+    {
+        private const string _cssToggleSwitchContainer = "form-check form-switch mt-2";
+
+        public ToggleSwitch()
+        {
+            Size = Size.None;
+        }
+
+        [Parameter]
+        public bool Checked { get; set; } = true;
+
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+
+        [Parameter]
+        public string ToggleColor { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback<bool> CheckedChanged { get; set; }
+
+        private string? ToggleBgColorStyle =>
+            string.IsNullOrEmpty(ToggleColor) ? null : $"background-color: {ToggleColor}";
+
+        private async Task OnCheckedChanged(ChangeEventArgs args)
+        {
+            if (args.Value is not null)
+            {
+                Checked = (bool)args.Value;
+                await CheckedChanged.InvokeAsync(Checked);
+            }
+        }
+
+        protected override string? CalculateCssClasses()
+        {
+            var result = new CssBuilder(_cssToggleSwitchContainer)
+                             .AddClass(ToggleSizeMetadata.GetSizeCss(Size))
+                             .AddClassFromAttributes(RemainingAttributes)
+                             .Build();
+            return result;
+        }
+
+        protected override string? CalculateCssStyles()
+        {
+            var result = new StyleBuilder()
+                .AddStyleFromAttributes(RemainingAttributes)
+                .Build();
+
+            return result;
+        }
+    }
+}
