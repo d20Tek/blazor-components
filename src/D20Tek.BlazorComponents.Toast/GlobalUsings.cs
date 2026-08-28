03edb94d2550acc91cdd274e054e611212fb2c75
Pedro Silva <DarthPedro@gmail.com>
2026-08-27 21:23:11 -0700

    Created the new D20Tek.BlazorComponents.Toast project to the solution.

---
+++ src/D20Tek.BlazorComponents.Toast/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using D20Tek.BlazorComponents.Utilities;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Diagnostics.CodeAnalysis;
